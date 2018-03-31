45ee4f363554f75a4ca873628d5dced4d9445320
coblan <coblan163.com>
2018-03-31 15:28:18 +0800

    vddf

--- try/scin/num/num_zhaoxiang.py
+++ try/scin/num/num_zhaoxiang.py
@@ -1,29 +1,10 @@
 import numpy as np
-import geo
+#import geo
 import math
-def millerXY(lon, lat):
-    L = 6381372 * math.pi * 2
-    W = L
-    H = L / 2
-    mill =1 # 2.3
-    x = lon * math.pi / 180
-    y = lat * math.pi / 180
-    y = 1.25 * math.log(math.tan( 0.25 * math.pi + 0.4 * y ))
-    x = ( W / 2 ) + ( W / (2 * math.pi) ) * x
-    y = ( H / 2 ) - ( H / ( 2 * mill ) ) * y
-    return (x,y)
 
-
-def XYmiller(x,y):
-    L = 6381372 * math.pi * 2
-    W = L
-    H = L / 2
-    mill = 1# 2.3    
-    lon=360/W * (x-W/2)
-    lat=180/(0.4*math.pi)*(math.atan( math.exp( (H-2*y)*mill/(1.25*H) ) ) -0.25*math.pi)
-    return lon,lat
-
-
+#################################
+##地图非线性映射
+##############################
 def mercator(lon,lat):
     x=lon*20037508.34/180
     y=math.log(math.tan((90+lat)*math.pi/360))/(math.pi/180)
@@ -36,16 +17,9 @@ def un_mercator(x,y):
     lat=180/math.pi*(2*math.atan(math.exp(y1*math.pi/180))-math.pi/2)
     return x1,lat
 
-def get_loc(x,y):
-    o_x=a*x+b*y+c
-    o_y=b*x-a*y+d
-    lon=o_x
-    lat=o_y
-    #lon,lat = XYmiller(o_x,o_y)
-    lon,lat = un_mercator(o_x,o_y)
-    #return geo.wgs84_to_gcj02( lon,lat )
-    return lon,lat
-
+#################################
+##从三高与经纬度坐标，推出映射矩阵a,b,c,d
+#################################
 ## 121.111432,31.152124  -34360,-8977
 ## 121.315323,31.175311  -14940,6437
 ## 121.160877,31.164138  -29640,7648
@@ -56,12 +30,12 @@ def get_loc(x,y):
 
 ##121.473052,31.233868  116.40,-39.92
 ##121.00467,30.963377 -44590,29830
-#################################
-x1,y1=-30330,6443
-mx1,my1= 121.153663,31.174931
 
-x2,y2= -40090,13670
-mx2,my2= 121.051354,31.109524
+#x1,y1=-30330,6443
+#mx1,my1= 121.153663,31.174931
+
+#x2,y2= -40090,13670
+#mx2,my2= 121.051354,31.109524
 
 
 x1,y1=-28405.38,-18155.34 
@@ -93,6 +67,45 @@ m=np.mat([[x1,y1,1,0],
 A = m.I * maped.T
 
 a,b,c,d = np.array(A.T)[0]
+
+######################################
+## 将三高转换为经纬度
+######################################
+
+#def millerXY(lon, lat):
+    #L = 6381372 * math.pi * 2
+    #W = L
+    #H = L / 2
+    #mill =1 # 2.3
+    #x = lon * math.pi / 180
+    #y = lat * math.pi / 180
+    #y = 1.25 * math.log(math.tan( 0.25 * math.pi + 0.4 * y ))
+    #x = ( W / 2 ) + ( W / (2 * math.pi) ) * x
+    #y = ( H / 2 ) - ( H / ( 2 * mill ) ) * y
+    #return (x,y)
+
+
+#def XYmiller(x,y):
+    #L = 6381372 * math.pi * 2
+    #W = L
+    #H = L / 2
+    #mill = 1# 2.3    
+    #lon=360/W * (x-W/2)
+    #lat=180/(0.4*math.pi)*(math.atan( math.exp( (H-2*y)*mill/(1.25*H) ) ) -0.25*math.pi)
+    #return lon,lat
+
+
+
+
+def get_loc(x,y):
+    o_x=a*x+b*y+c
+    o_y=b*x-a*y+d
+    lon=o_x
+    lat=o_y
+    #lon,lat = XYmiller(o_x,o_y)
+    lon,lat = un_mercator(o_x,o_y)
+    #return geo.wgs84_to_gcj02( lon,lat )
+    return lon,lat
 
 
 print(get_loc(-6538,-19020))
